b74cf0a96d393af2a2c4c6353e6417063ffe3086
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-06-29 21:33:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HW2
+{
+    class program1
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age, num;
+            char gender; 
+            Console.WriteLine("How many students do you wanna register?");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("write name for student " + (i + 1));
+                name = Console.ReadLine();
+                Console.WriteLine("write age for student " + (i + 1));
+                age = int.Parse(Console.ReadLine());
+                if (age < 0)
+                {
+                    Console.WriteLine("Age cannot be less than 0. Write Again");
+                    age = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("write gender for student(F or M) " + (i + 1));
+                gender = char.Parse(Console.ReadLine());
+                if(gender != 'F'|| gender != 'M')
+                {
+                    Console.WriteLine("Write F or M");
+                    gender = char.Parse(Console.ReadLine());
+                }
+            }
+
+
+
+        }
+    }
+}
